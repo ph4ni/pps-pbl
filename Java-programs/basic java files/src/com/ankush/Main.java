802c2097002695566780955a15dfbea8bebf83cf
Ankush Mehta <ankush.m3103@gmail.com>
2021-10-24 20:35:58 +0530

    adding java files

---
+++ Java-programs/basic java files/src/com/ankush/Main.java
@@ -0,0 +1,13 @@
+package com.ankush;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("uhm hey");
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}
