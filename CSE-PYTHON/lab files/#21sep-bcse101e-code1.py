63caeb5b682f91b17e6d4fc95d7d1d156366bd1a
Ankush Mehta <ankush.m3103@gmail.com>
2021-10-24 21:23:51 +0530

    adding bcse101l files

---
+++ CSE-PYTHON/lab files/#21sep-bcse101e-code1.py
@@ -0,0 +1,9 @@
+#21sep-bcse101e-code1
+#radius of circle
+
+radius = float(input("Enter radius of circle for which area has to be computed"))
+square = radius**2
+
+area = 3.14*square
+
+print(area)
---
+++ CSE-PYTHON/untitled2.py
@@ -0,0 +1,7 @@
+#convert octal to binary
+
+oct_num = int(input("enter: "))
+
+bin_num = bin(oct_num)
+#print binary Number
+print('Number after conversion is :' + str(bin_num))
